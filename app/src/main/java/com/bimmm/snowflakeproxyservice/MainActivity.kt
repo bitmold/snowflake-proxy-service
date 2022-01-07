650b10af91edd3aa15317e2966983089881cddfa
b <dsnake@protonmail.com>
2022-01-07 18:03:33 -0500

    cleaning

--- app/src/main/java/com/bimmm/snowflakeproxyservice/MainActivity.kt
+++ app/src/main/java/com/bimmm/snowflakeproxyservice/MainActivity.kt
@@ -12,7 +12,7 @@ import androidx.appcompat.widget.SwitchCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var tvCount : TextView
+    private lateinit var tvCount: TextView
 
     private var count = -1
 
@@ -27,18 +27,20 @@ class MainActivity : AppCompatActivity() {
             var intent = Intent(this, SnowflakeProxyService::class.java)
                 .setAction(SnowflakeProxyService.ACTION_START)
                 .putExtra(SnowflakeProxyService.EXTRA_START_CHECK_POWER, checkPowerRadio.isChecked)
-                .putExtra(SnowflakeProxyService.EXTRA_START_CHECK_UNMETERED, checkMeteredRadio.isChecked)
+                .putExtra(
+                    SnowflakeProxyService.EXTRA_START_CHECK_UNMETERED,
+                    checkMeteredRadio.isChecked
+                )
             startService(intent)
             checkPowerRadio.isEnabled = false
             checkMeteredRadio.isEnabled = false
         }
 
-        with(LocalBroadcastManager.getInstance(this)) {
+        LocalBroadcastManager.getInstance(this).apply {
             registerReceiver(receiver, IntentFilter(SnowflakeProxyService.ACTION_CLIENT_CONNECTED))
             registerReceiver(receiver, IntentFilter(SnowflakeProxyService.ACTION_PAUSING))
             registerReceiver(receiver, IntentFilter(SnowflakeProxyService.ACTION_RESUMING))
         }
-
 
         updateCount()
     }
@@ -49,18 +51,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     private var receiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context?, intent: Intent?) {
-            var action: String? = intent?.action ?: return
-            if (action == SnowflakeProxyService.ACTION_CLIENT_CONNECTED) {
+        override fun onReceive(context: Context?, intent: Intent?) = when (intent?.action) {
+            SnowflakeProxyService.ACTION_CLIENT_CONNECTED -> {
                 updateCount()
-            } else if (action == SnowflakeProxyService.ACTION_PAUSING) {
+            }
+            SnowflakeProxyService.ACTION_PAUSING -> {
                 var reason = intent.getStringExtra(SnowflakeProxyService.EXTRA_PAUSING_REASON)
                 Toast.makeText(context, "Pausing proxy: $reason", Toast.LENGTH_LONG).show()
-            } else if (action == SnowflakeProxyService.ACTION_RESUMING) {
+            }
+            SnowflakeProxyService.ACTION_RESUMING -> {
                 Toast.makeText(context, "Proxy resuming", Toast.LENGTH_LONG).show()
             }
+            else -> {
+            }
         }
-
     }
 
     private fun updateCount() {
